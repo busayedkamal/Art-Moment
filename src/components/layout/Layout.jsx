3f9ddc5fc2884b6d28427771cc6ba7696e2431b3
fit-moment <abuabdulameer2020@gmail.com>
2026-05-10 23:03:07 +0300

    wedth

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -110,7 +110,7 @@ export default function Layout() {
         </header>
 
         {/* Page Content */}
-        <main className="flex-1 overflow-y-auto p-4 md:p-8">
+        <main className="flex-1 overflow-y-auto overflow-x-hidden p-4 md:p-8">
           <Outlet />
         </main>
       </div>
